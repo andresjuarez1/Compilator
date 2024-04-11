35aeaa0a78833274fe4639edcf8c71695c332347
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2024-04-10 19:23:20 -0600

    fix

--- src/analyzer/interpreter/interprete.js
+++ src/analyzer/interpreter/interprete.js
@@ -1,25 +1,31 @@
-import {peg$parse} from "./translater.js";
+import { peg$parse } from "./translater.js";
 import Swal from "sweetalert2";
 
-export function translate(codigoFuente){
-    const generateCod = peg$parse(codigoFuente.replace(/\s+/g, ' '))
+export function translate(codigoFuente) {
+    try {
+        const generateCod = peg$parse(codigoFuente.replace(/\s+/g, ' '))
 
-    if (generateCod){
-        Swal.fire({
-            title: "Código correcto!",
-            text: codigoFuente,
-            icon: "success"
-        });
-        const capturedOutput = captureOutput(() => {
-            eval(generateCod);
-        });
-        showResult(capturedOutput);
+        if (generateCod) {
+            Swal.fire({
+                title: "Código correcto!",
+                text: codigoFuente,
+                icon: "success"
+            });
+            const capturedOutput = captureOutput(() => {
+                eval(generateCod);
+            });
+            showResult(capturedOutput);
+        }
+    } catch(e) {
+        console.error("ERROR :", e);
+        const error = document.getElementById('Salida')
+        error.innerText = e
     }
 
     function captureOutput(callback) {
         let capturedOutput = '';
         const originalConsoleLog = console.log;
-        console.log = function(message) {
+        console.log = function (message) {
             capturedOutput += message + '\n';
         };
         callback();
@@ -30,7 +36,7 @@ export function translate(codigoFuente){
         const resultadoElemento = document.getElementById('salida');
         resultadoElemento.innerText = result;
     }
-}   
+}
 
 
 
